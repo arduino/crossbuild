86143da3c98710810fb7c8e4dd6fc32743d99c49
Cristian Maglie <c.maglie@bug.st>
2026-07-17 18:46:16 +0200

    Refactored build ARGS

--- arch/build_all.sh
+++ arch/build_all.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-VERSION=ubuntu-22.04-1
-docker build -f Dockerfile-base          -t ghcr.io/arduino/crossbuild-base:$VERSION .
+VERSION=ubuntu-24.04-1
+docker build -f Dockerfile-base \
+             -t ghcr.io/arduino/crossbuild-base:$VERSION .
 docker build --build-arg BASE_VERSION=$VERSION \
-             -f Dockerfile-linux-amd64   -t ghcr.io/arduino/crossbuild-x86_64-linux-gnu:$VERSION .
+             -f Dockerfile-linux-amd64 \
+             -t ghcr.io/arduino/crossbuild-linux-amd64:$VERSION .
 docker build --build-arg BASE_VERSION=$VERSION \
-             -f Dockerfile-windows-amd64 -t ghcr.io/arduino/crossbuild-x86_64-w64-mingw32:$VERSION .
+             -f Dockerfile-windows-amd64 \
+             -t ghcr.io/arduino/crossbuild-windows-amd64:$VERSION .
 docker build --build-arg BASE_VERSION=$VERSION \
-             -f Dockerfile-windows-arm64 -t ghcr.io/arduino/crossbuild-aarch64-w64-mingw32:$VERSION .
+             -f Dockerfile-windows-arm64 \
+             -t ghcr.io/arduino/crossbuild-windows-arm64:$VERSION .
