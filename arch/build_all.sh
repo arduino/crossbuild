8642193be0cdc10580232512a53ddf4f118c3d66
Cristian Maglie <c.maglie@bug.st>
2026-07-20 02:12:13 +0200

    Added builds for all OS/ARCH combination

--- arch/build_all.sh
+++ arch/build_all.sh
@@ -1,10 +1,14 @@
-#!/bin/sh
+#!/bin/sh -ex
 
+# Build all the crossbuild base images for Arduino
 docker build --build-arg BASE_IMAGE=ubuntu:16.04 -f Dockerfile-base -t ghcr.io/arduino/crossbuild-base:ubuntu-16.04-1 .
 docker build --build-arg BASE_IMAGE=ubuntu:18.04 -f Dockerfile-base -t ghcr.io/arduino/crossbuild-base:ubuntu-18.04-1 .
 docker build --build-arg BASE_IMAGE=ubuntu:20.04 -f Dockerfile-base -t ghcr.io/arduino/crossbuild-base:ubuntu-20.04-1 .
 docker build --build-arg BASE_IMAGE=ubuntu:24.04 -f Dockerfile-base -t ghcr.io/arduino/crossbuild-base:ubuntu-24.04-1 .
+LATEST=ubuntu-24.04-1
 
+# Linux crossbuild images
+# -----------------------
 VERSION=ubuntu-16.04-1
 docker build -f Dockerfile-linux-all \
              --build-arg BASE_VERSION=$VERSION \
@@ -23,7 +27,17 @@ docker build -f Dockerfile-linux-all \
              --build-arg DEB_PACKAGES="gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf" \
              -t ghcr.io/arduino/crossbuild-linux-armhf:$VERSION .
 
-VERSION=ubuntu-24.04-1
+# i686 compiler is available from 18.04 onwards
+VERSION=ubuntu-18.04-1
+docker build -f Dockerfile-linux-all \
+             --build-arg BASE_VERSION=$VERSION \
+             --build-arg CROSS_COMPILE=i686-linux-gnu \
+             --build-arg DEB_PACKAGES="gcc-i686-linux-gnu g++-i686-linux-gnu" \
+             -t ghcr.io/arduino/crossbuild-linux-i686:$VERSION .
+
+# macOS crossbuild images
+# -----------------------
+VERSION=$LATEST
 docker build -f Dockerfile-macos-all \
              --build-arg BASE_VERSION=$VERSION \
              --build-arg CROSS_COMPILE=aarch64-apple-darwin25.1 \
@@ -34,10 +48,9 @@ docker build -f Dockerfile-macos-all \
              --build-arg CROSS_COMPILE=x86_64-apple-darwin25.1 \
              -t ghcr.io/arduino/crossbuild-macos-amd64:$VERSION .
 
-docker build -f Dockerfile-linux-amd64 \
-             --build-arg BASE_VERSION=$VERSION \
-             -t ghcr.io/arduino/crossbuild-linux-amd64:$VERSION .
-
+# Windows crossbuild images
+# -------------------------
+VERSION=$LATEST
 docker build -f Dockerfile-windows-all \
              --build-arg BASE_VERSION=$VERSION \
              --build-arg CROSS_COMPILE=x86_64-w64-mingw32 \
